d14cc82b08cf073ab972c107fa6877d6ba1fa5ec
Vyshnavi9989 <ankireddypallivyshnavi9989@gmail.com>
2022-07-18 10:52:14 +0530

    addition

---
+++ basic/session/src/com/simplilearn/demo/Addition.java
@@ -0,0 +1,23 @@
+package com.simplilearn.demo;
+import java.util.Scanner;
+public class Addition {
+public static void main(String[] args) {
+		
+		
+		Scanner  sc = new Scanner(System.in);
+		
+		int num1,num2;
+		
+		System.out.println("Enter First Number");
+		num1=sc.nextInt();
+		
+		System.out.println("Enter Second Number");
+		num2=sc.nextInt();
+		
+		int result=num1+num2;
+		
+		System.out.println("Addition is: "+result);
+       }
+		
+
+}
